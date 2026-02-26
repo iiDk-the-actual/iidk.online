b8dfe08a22dd307c3330ef23b14ac9859d66f38f
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2026-02-26 17:12:08 -0500

    console.log fix

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const http = require('http');
 const server = http.createServer(async (req, res) => {
     try {
         const clientIp = req.headers['cf-connecting-ip'] || req.socket.remoteAddress;
-        const ipHash = hashIpAddr(clientIp);
 
-        console.log(`${ipHash} ${req.method} ${req.url}`);
+        console.log(`${clientIp} ${req.method} ${req.url}`);
         res.writeHead(500).end(JSON.stringify({ status: 500, message: "I don't really know what to do with this URL anymore" })); return;
     } catch (err) {
         console.error('Error processing request:', err.message);
