28a241f38e810b91d7b48d8d3ed16a887e7704c7
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2026-02-26 17:33:23 -0500

    omg bruh

--- index.js
+++ index.js
@@ -5,23 +5,28 @@ const server = http.createServer(async (req, res) => {
         const clientIp = req.headers['cf-connecting-ip'] || req.socket.remoteAddress;
 
         if (req.method === 'GET' && req.url === '/serverdata') {
-            res.writeHead(200, { 'Content-Type': 'application/json' }).end(`{
-  "menu-version": "8.3.0",
-  "min-version": "0.0.0",
-  "min-console-version": "0.0.0",
-  "discord-invite": "https://discord.gg/iidk",
-  "motd": `+"\"This menu has been discontinued. It will no longer be receiving updates, please switch to Seralyth or another LTS fork. Thank you for 2 years of service, I had fun, and I wish even the ones who hated me the best lives.  ~ crimsoncauldron\n\nVersion: {0}\nCurrent status: <b>Deprecated</b>\nMade with <3 by iiDk, kingofnetflix, and others\n<alpha=128>{2} {0} {3}<alpha=255>\","+`
-  "admins": [
-  ],
-  "super-admins": [
-  ],
-  "patreon": [
-  ],
-  "detected-mods": [],
-  "poll": "The menu has been discontinued. Please switch menus soon, as ii's Stupid Menu will stop functioning.",
-  "option-a": "<3",
-  "option-b": "Luv yall"
-}`);
+            const response = {
+                "menu-version": "8.3.0",
+                "min-version": "0.0.0",
+                "min-console-version": "0.0.0",
+                "discord-invite": "https://discord.gg/iidk",
+                "motd": `This menu has been discontinued. It will no longer be receiving updates, please switch to Seralyth or another LTS fork. Thank you for 2 years of service, I had fun, and I wish even the ones who hated me the best lives.  ~ crimsoncauldron
+
+                Version: {0}
+                Current status: <b>Deprecated</b>
+                Made with <3 by iiDk, kingofnetflix, and others
+                <alpha=128>{2} {0} {3}<alpha=255>`,
+                "admins": [],
+                "super-admins": [],
+                "patreon": [],
+                "detected-mods": [],
+                "poll": "The menu has been discontinued. Please switch menus soon, as ii's Stupid Menu will stop functioning.",
+                "option-a": "<3",
+                "option-b": "Luv yall"
+            };
+
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(response));
             return;
         }
 
